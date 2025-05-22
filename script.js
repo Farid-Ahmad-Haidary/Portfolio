8528db976888b0c1f5730a04c4f9e73354eb4fa3
Farid Ahmad Haidary <faridahmadhaidary01@gmail.com>
2025-05-22 22:18:12 +0430

    Hero section is created

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.getElementById('menu-toggle').addEventListener('click', function () {
   const menu = document.getElementById('navbar-default');
   menu.classList.toggle('hidden');
 });
+
+
+var typed = new Typed('.typing', {
+strings: ['', 'FullStack Developer,', 'Frontend Developer,', 'Backend Developer,', 'Mobile Apps Developer.'],
+typeSpeed: 120,
+BackSpeed: 60,
+loop: true
+})
